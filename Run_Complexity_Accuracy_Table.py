9ddd07c00c27a0c828a09e81d15bda88cdba578e
MelikaSepidband <97177956+MelikaSepidband@users.noreply.github.com>
2024-11-21 18:22:23 -0500

    Update Run_Complexity_Accuracy_Table.py

--- Run_Complexity_Accuracy_Table.py
+++ Run_Complexity_Accuracy_Table.py
@@ -9,7 +9,7 @@ def main():
     train = input("Is it a train list? (If yes Enter 1 if not Enter 0)")
     codes=generate_code(data_name=data_name, model_name=model_name, train=train)
     if data_name == 'HumanEval' or data_name == 'leetcode':
-        testcases=oringinal_test_cases(data_name)
+        testcases=oringinal_test_cases(data_name=data_name)
     else:
         testcases = None
     df = compute_complexity(data_name=data_name, testcases=testcases, codes=codes)

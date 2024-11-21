9dc8da1c36370a925bf697702c47827b7234fe5a
MelikaSepidband <97177956+MelikaSepidband@users.noreply.github.com>
2024-11-21 18:22:35 -0500

    Update Run_Distribution_BarPlot.py

--- Run_Distribution_BarPlot.py
+++ Run_Distribution_BarPlot.py
@@ -7,12 +7,12 @@ def main():
     data_name = input("Enter the dataset")
     model_name = input("Enter the model")
     train = input("Is it a train list? (If yes Enter 1 if not Enter 0)")
-    codes=generate_code(data_name, model_name, train)
+    codes=generate_code(data_name=data_name, model_name=model_name, train=train)
     if data_name == 'HumanEval' or data_name == 'leetcode':
-        testcases=oringinal_test_cases(data_name)
+        testcases=oringinal_test_cases(data_name=data_name)
     else:
         testcases = None
-    plot=distribution(data_name, testcases, codes)
+    plot=distribution(data_name=data_name, testcases=testcases, codes=codes)
     return plot
 
 if __name__ == "__main__":

f6d6722124145c8a7d7a52eea6de7ca04d00cdae
MelikaSepidband <97177956+MelikaSepidband@users.noreply.github.com>
2025-03-22 17:01:20 -0500

    Update Run_Complexity_Accuracy_Table.py

--- Run_Complexity_Accuracy_Table.py
+++ Run_Complexity_Accuracy_Table.py
@@ -1,14 +1,14 @@
 from complexity_accuracy import *
 
 def main():
-    #datasets = ['HumanEval', 'mbpp', 'leetcode']
-    #models = ['gpt-4o', 'gpt-3.5-turbo', 'llama3.1']
+    #datasets = ['HumanEval', 'mbpp', 'leetcode', 'BigCodeBench]
+    #models = ['gpt-4o', 'gpt-3.5-turbo', 'llama3.1', 'o3-mini']
     
     data_name = input("Enter the dataset: ")
     model_name = input("Enter the model: ")
     train = input("Is it a train list? (If yes Enter 1 if not Enter 0): ")
     codes=generate_code(data_name=data_name, model_name=model_name, train=int(train))
-    if data_name == 'HumanEval' or data_name == 'leetcode':
+    if data_name == 'HumanEval' or data_name == 'leetcode' or data_name=='BigCodeBench':
         testcases=oringinal_test_cases(data_name=data_name)
     else:
         testcases = None
